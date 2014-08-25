e398f53a23f04c1707369db495f84fcb283603dd
Ian Blenke <ian@blenke.com>
2014-08-25 01:02:57 -0400

    Adding consul support

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ if [ -n "${ETCD_HOST:+x}" ]; then
   mv /etc/supervisor/supervisord.conf /tmp/supervisord.conf
   sed -e "/confd -node/s/127.0.0.1:4001/${ETCD_HOST}/" /tmp/supervisord.conf > /etc/supervisor/supervisord.conf
 fi
+# Run docker run with -e CONSUL_HOST=<ip>:<port>
+if [ -n "${CONSUL_HOST}" ]; then
+  mv /etc/supervisor/supervisord.conf /tmp/supervisord.conf
+  sed -e "s/confd -node=\"http://127.0.0.1:4001\"/-consul -consul-addr ${CONSUL_HOST}/" /tmp/supervisord.conf > /etc/supervisor/supervisord.conf
+fi
 
 # for debugging
 cat /etc/supervisor/supervisord.conf
